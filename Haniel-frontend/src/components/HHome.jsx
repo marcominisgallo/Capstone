cb780925749a8bb7fdf0e44cd755921b14091faa
Marco <marcominisgallo@yahoo.it>
2025-07-14 16:58:22 +0200

    Update home page to redirect unauthenticated users
    Replaces static booking link with conditional routing: unauthenticated users are redirected to the login page when clicking 'Prenota ora!'. Also cleans up unused image imports and corrects logo path.

--- Haniel-frontend/src/components/HHome.jsx
+++ Haniel-frontend/src/components/HHome.jsx
@@ -1,15 +1,12 @@
-import { Button, Carousel, Col, Image, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Button, Row, Col, Image } from "react-bootstrap";
 import HMax from "./HMax";
 import image1 from "../assets/pic/image15.jpeg";
-import image2 from "../assets/pic/image2.jpeg";
-import image3 from "../assets/pic/image3.jpeg";
-import image4 from "../assets/pic/image4.jpeg";
-import image5 from "../assets/pic/image5.jpeg";
-import image6 from "../assets/pic/image6.jpeg";
-import image7 from "../assets/pic/image7.jpeg";
-import { Link } from "react-router-dom";
 
 function HHome() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
     <HMax>
       <Row className="mb-5 mb-lg-3 ">
@@ -19,12 +16,12 @@ function HHome() {
           lg={3}
           className="offset-2 offset-md-3 offset-lg-2 d-none d-lg-block"
         >
-          <img src="./HanielLogo.svg" alt="logo" className="w-100" />
+          <img src="/HanielLogo.svg" alt="logo" className="w-100" />
         </Col>
         <Col xs={10} lg={6} className="offset-2 offset-lg-1 mt-5">
           <div id="HomePic">
             <Image src={image1} className="w-75 mt-5 rounded-2" />
-            <Link to="/prenota">
+            <Link to={isAuthenticated ? "/prenota" : "/login"}>
               <Button id="PrenotaButton" className="py-3 px-3 rounded-pill">
                 Prenota ora!
               </Button>
